3158cc93e8b77811a417525def21324f9654f080
Yaros-Lav <62016334+Yaros1113@users.noreply.github.com>
2025-05-22 16:03:18 +1000

    init angular

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login.component';
+import { RegisterComponent } from './auth/register.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ProductsComponent } from './products/products.component';
+import { TodoComponent } from './todo/todo.component';
+
+export const routes: Routes = [];
